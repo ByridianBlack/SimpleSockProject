ba00e03650076a871a21c939875d2b1f55bcc510
ahwassa10 <71190521+ahwassa10@users.noreply.github.com>
2022-01-30 15:50:03 -0500

    Fixed server so that it reversed lines, not the whole input

--- server.py
+++ server.py
@@ -21,12 +21,38 @@ print("[S]: Server IP address is {}".format(localhost_ip))
 csockid, addr = ss.accept()
 print ("[S]: Got a connection request from a client at {}".format(addr))
 
-    # send a intro message to the client.
-recieved_data = ""
-data_temp = ""
-
-
+# send a intro message to the client.
+msg = "Welcome to CS 352!"
+csockid.send(msg.encode('utf-8'))
 csockid.settimeout(25)
+
+data_buffer = ""
+buffer_iter = 0
+line_buffer = ""
+
+output = open("out-proj.txt", 'w');
+while True:
+    try:
+        data_buffer += csockid.recv(512).decode("utf-8")
+        
+        while buffer_iter < (len(data_buffer) - 1):
+            atChar = data_buffer[buffer_iter]
+            if atChar == '\n':
+                output.write(line_buffer.reverse())
+                line_buffer = ""
+            else:
+                line_buffer += atChar
+            
+            buffer_iter += 1
+    
+    except:
+        break
+
+# Close the server socket
+ss.close()
+exit()     
+
+'''
 while True:
     try:
         data_temp = csockid.recv(200).decode("utf-8")
@@ -36,13 +62,4 @@ while True:
     except socket.timeout:
         break
 
-    
-
-
-
-msg = "Welcome to CS 352!"
-csockid.send(msg.encode('utf-8'))
-
-    # Close the server socket
-ss.close()
-exit()
+'''
